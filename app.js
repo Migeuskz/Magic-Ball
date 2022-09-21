5f062d255023130e71ad47b17988197139d37bd1
Migeuskz <msanchezpapalotzi@gmail.com>
2022-09-21 12:33:46 -0500

    acabando la app de la bola mágica

---
+++ app.js
@@ -0,0 +1,53 @@
+const ask = () =>{
+    //alert('Wasaaaaaa!');
+    //leer pregunta
+    const questionInput = document.getElementById("question");
+    if(questionInput.value !==""){
+
+    //recibir respuesta al preguntar a la bola mágica
+    let answer = magicBall();
+    //Imprimir resultado
+    const answerInput = document.getElementById('answer');
+    answerInput.value =answer;
+}else{
+    alert("A donde crées que vas imbecil >:(");
+    answerInput.value = "";
+}
+};
+
+//Función Bola Mágica 
+const magicBall = () =>{
+    let randomNumber = Math.floor(Math.random()*8);
+    let eightBall = '';
+    switch(randomNumber){
+        
+        case 0:
+            eightBall = 'Si';
+            break;
+        
+        case 1:
+            eightBall = 'No';
+            break;
+        
+        case 2:
+            eightBall = 'Talves';
+            break;
+        
+        case 3:
+            eightBall = 'Probablemente';
+            break;
+        case 4:
+            eightBall = 'No lo se';
+            break;                        
+        case 5:
+            eightBall = 'xD';
+            break;
+        case 6:
+            eightBall = 'No se tu dimelo';
+            break;
+        case 7:
+            eightBall = 'adios mi planeta me necesita';
+            break;    
+    }
+    return eightBall;
+;}
